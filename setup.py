f87773292ac8d5f555f1b9834cb32b601fb094d3
vidyasagar150797 <vidyasagarsasumana1997@gmail.com>
2022-07-02 15:31:23 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description= "its a WineQ package",
+    author= "vidyasagar150797",
+    packages= find_packages(),
+    license="MIT"
+
+)
